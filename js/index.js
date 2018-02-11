7503d3cf6d721e55f0fd79401f0bcd07ff1fe345
asela1982 <asela.d82@gmail.com>
2018-02-10 22:59:32 -0600

    add pagination

--- js/index.js
+++ js/index.js
@@ -2,17 +2,76 @@
 var $tbody = document.querySelector("tbody");
 var $btnSearch = document.getElementById("search");
 var $btnReset = document.getElementById("reset");
-var $records = document.getElementById("records");
+var $ul = document.querySelector("ul");
 
-// store the data to a variable
-var filteredDataSet = dataSet;
-
-// render the table for the first time on page load
-renderTable();
 
 // add event listerns to the buttons
 $btnSearch.addEventListener("click", filterByParameters);
 $btnReset.addEventListener("click", resetTable);
+$ul.addEventListener("click", clickPage);
+
+// Pagination
+var itemsPerPage = 1000;
+
+var filteredDataSet = dataSet;
+
+// function which does the pagination
+function clickPage(event){
+    var pageNumber = event.target.parentNode.id;
+
+    if (pageNumber === 1) {
+        var startIndex = 0;
+        var endIndex = startIndex + itemsPerPage;
+    
+        console.log(startIndex);
+        console.log(endIndex);
+    
+        filteredDataSet = filteredDataSet.slice(startIndex, endIndex);
+    
+        renderTable();
+        filteredDataSet = dataSet;
+
+    }  else  {
+        var startIndex = (pageNumber-1)*itemsPerPage;
+        var endIndex = startIndex + itemsPerPage;
+    
+        console.log(startIndex);
+        console.log(endIndex);
+    
+        filteredDataSet = filteredDataSet.slice(startIndex, endIndex);
+    
+        renderTable(filteredDataSet);
+        filteredDataSet = dataSet;
+    }  
+};
+
+// funciton for pagination
+function pagination(data){
+    // store the data to a variable
+
+    $ul.innerHTML = "";
+    console.log(data.length)
+
+    var numberOfPages = data.length / itemsPerPage;
+
+
+    for (var i = 1; i <= numberOfPages; i++) {
+        var $li = document.createElement("li");
+        $li.setAttribute("id", i);
+        $li.innerHTML = `<a href="#">${i}</a>`;
+        $ul.appendChild($li);
+    };
+
+    var startIndex = 0;
+    var endIndex = startIndex + itemsPerPage;
+
+    data = data.slice(startIndex, endIndex);
+ 
+    // render the table for the first time on page load
+    renderTable(data);
+};
+
+
 
 
 // function which filters the data based on user inputs
@@ -38,31 +97,29 @@ function filterByParameters(event) {
         // check if all elements in the filterArray are true or false
         // if true it will get filtered
         return filterArray.every(element => element === true)
-    })
+    });
 
-    // call the renderTable function to populate the data to the table in html
-    renderTable()
+    pagination(filteredDataSet)
 
 };
 
 
 // create a function which renders the table to the HTML
-function renderTable() {
+function renderTable(data) {
     // empty the content in the tbody element(using innerHTML)
     $tbody.innerHTML = "";
 
     // loop to add rows and cells based on the dimensions in dataSet
     // array of objects(insertRow and insertCell)
-    for (i = 0; i < filteredDataSet.length; i++) {
+    for (i = 0; i < data.length; i++) {
         var $row = $tbody.insertRow(i);
 
-        for (j = 0; j < Object.keys(filteredDataSet[i]).length; j++) {
+        for (j = 0; j < Object.keys(data[i]).length; j++) {
             var $cell = $row.insertCell(j);
-            var cellValue = filteredDataSet[i][Object.keys(filteredDataSet[i])[j]];
+            var cellValue = data[i][Object.keys(data[i])[j]];
             $cell.innerText = cellValue;
         }
     }
-    $records.innerText = filteredDataSet.length
 };
 
 
@@ -77,7 +134,7 @@ function createUserInputObject() {
     var inputState = document.querySelector('#inputState');
     var inputCountry = document.querySelector('#inputCountry');
     var inputShape = document.querySelector('#inputShape');
-    
+
     if (inputDate.value !== "") {
         // the variable is defined
         userInputObject["datetime"] = inputDate.value;
@@ -116,9 +173,11 @@ function resetTable() {
     inputCountry.value = "";
     inputShape.value = "";
 
-    filteredDataSet = dataSet;
-    renderTable();
+    data = dataSet;
+
+    pagination(data)
+
 };
 
 
-
+pagination(filteredDataSet);
